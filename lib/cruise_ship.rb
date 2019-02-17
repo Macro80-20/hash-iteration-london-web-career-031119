de1fadd501acd9d99a6d930e57952394cd134ee7
fernando <fernando_lopezc@live.com>
2019-02-17 13:48:55 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,8 +7,10 @@
 # }
 
 def select_winner(passengers)
-  passengers.each do |key,value|
+
+winner = passengers.each do |key,value|
     if key == :suite_a && value.start_with("A")
       winner = value
     end
+  end
 end
